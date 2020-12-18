adce47a00703ad8a08ecf6d930e4bd51deca9582
Kasra K <seekasra@gmail.com>
2020-12-18 15:28:31 +0000

    add example

---
+++ example1.py
@@ -0,0 +1,21 @@
+import wizzer 
+
+q1 = [
+        'hostname',
+        'Username',
+        'Interface Config',
+        'Static Route',
+        'Routing protocol',
+]
+
+q2 = {
+        'hostname': '',
+        'Username': '',
+        'Interface Config': '',
+        'Static Route': '',
+        'Routing protocol': '',
+}
+
+q = wizzer.ask(q1)
+wizzer.review(q)
+
