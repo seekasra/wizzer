77aa57a67919de5540a2c6c092c0697bd27382ad
mohammad <mohammad.abolhasany@gmail.com>
2020-12-21 15:07:07 +0330

    update init.py

--- wizzer/__init__.py
+++ wizzer/__init__.py
@@ -3,8 +3,4 @@
 """This module is a wizard builder for setting up parameters 
 [ e.g. variable(s) / configuration(s) ] to run a service."""
 
-from .convert import convert
-from .ask import ask
-from .review import review
-__all__ = ["ask", "review"]
 
