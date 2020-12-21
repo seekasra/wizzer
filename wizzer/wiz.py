7e4e8ca7502c09a36e001adea7ca0247911fe8d8
mohammad <mohammad.abolhasany@gmail.com>
2020-12-21 15:16:09 +0330

    Update wiz.py

--- wizzer/wiz.py
+++ wizzer/wiz.py
@@ -1,4 +1,3 @@
-from . import convert
 class Wiz:
   def ask(questions):
       dict_q = convert(questions)
@@ -8,6 +7,15 @@ class Wiz:
       return dict_q
     
   def review(questions):
-  dict_q = convert(questions)
-  for q, a in dict_q.items():
-      print(str(q) + " :  " + str(a))
+    dict_q = convert(questions)
+    for q, a in dict_q.items():
+        print(str(q) + " :  " + str(a))
+      
+  def __convert(lst):
+    if isinstance(lst, list):
+        res_dct = {lst[i]: '' for i in range(0, len(lst))}
+    elif isinstance(lst, str):
+        res_dct = {lst: ''} 
+    else:
+        res_dct = lst
+    return res_dct 
