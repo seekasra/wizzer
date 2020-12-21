c349058c9c01af78966bf0c13c26d2f39d77151a
mohammad <mohammad.abolhasany@gmail.com>
2020-12-21 15:14:21 +0330

    Update wiz.py

--- wizzer/wiz.py
+++ wizzer/wiz.py
@@ -1,1 +1,8 @@
-
+from . import convert
+class Wiz:
+  def ask(questions):
+      dict_q = convert(questions)
+      for q, a in dict_q.items():
+          a = input("What's the " + str(q) + " ?  ")
+          dict_q[q] = a
+      return dict_q
